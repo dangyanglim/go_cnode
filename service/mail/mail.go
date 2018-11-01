314d385e2b204d57b4c70d737153628c5c3dd640
unknown <CHENYL6@cn.midea.com>
2018-11-01 14:36:04 +0800

    邮箱改用465

--- service/mail/mail.go
+++ service/mail/mail.go
@@ -2,175 +2,90 @@ package mail
 
 import (
 	"fmt"
-	"log"
-	"net/http"
+	//"log"
+	//"net/http"
 	"net/smtp"
-	"regexp"
+	//"regexp"
 	"strings"
 
 	"github.com/dangyanglim/go_cnode/mgoModels"
-	"github.com/gin-gonic/gin"
-	"github.com/tommy351/gin-sessions"
+	//"github.com/gin-gonic/gin"
+	//"github.com/tommy351/gin-sessions"
+	"crypto/tls"
+
+	"log"
+	"net"
+
+	
 )
 
 var userModel = new(models.UserModel)
 
-func ShowSignup(c *gin.Context) {
-
-	c.HTML(http.StatusOK, "showsignup", gin.H{
-		"title": "布局页面",
-		"config": gin.H{
-			"description": "CNode：Node.js专业中文社区",
-		},
-	})
-}
-func Signin(c *gin.Context) {
-
-	c.HTML(http.StatusOK, "signin", gin.H{
-		"title": "布局页面",
-		"config": gin.H{
-			"description": "CNode：Node.js专业中文社区",
-		},
-	})
-}
-func Signout(c *gin.Context) {
-	session := sessions.Get(c)
-	session.Clear()
-	session.Save()
-	c.Redirect(301, "/")
-}
-
-func Login(c *gin.Context) {
-	name := c.Request.FormValue("name")
-	//pass := c.Request.FormValue("pass")
-	user, _ := userModel.GetUserByName(name)
-	session := sessions.Get(c)
-	session.Set("loginname", user.Loginname)
-	session.Set("accessToken", user.AccessToken)
-	session.Save()
-	log.Println(user.Loginname)
-	// var no_reply_topics =[]string{"2","2"};
-	// var tops =[]string{"2","2"};
-	tab := c.Request.FormValue("tab")
-
-	if tab == "" {
-		tab = "all"
+func Dial(addr string) (*smtp.Client, error) {
+	conn, err := tls.Dial("tcp", addr, nil)
+	if err != nil {
+		log.Println("Dialing Error:", err)
+		return nil, err
 	}
 
-	//c.Redirect(http.StatusMovedPermanently, "http://shanshanpt.github.io/")
-	// c.HTML(http.StatusOK, "index", gin.H{
-	// 	"title": "布局页面",
-	// "no_reply_topics":no_reply_topics,
-	// "user":user,
-	// "tops":tops,
-	// "tab":tab,
-	// 	"config": gin.H{
-	// 		"description": "CNode：Node.js专业中文社区",
-	// 	},
-	// })
-	c.Redirect(301, "/")
+	host, _, _ := net.SplitHostPort(addr)
+	return smtp.NewClient(conn, host)
 }
-func SearchPass(c *gin.Context) {
 
-	c.HTML(http.StatusOK, "search_pass", gin.H{
-		"title": "布局页面",
-		"config": gin.H{
-			"description": "CNode：Node.js专业中文社区",
-		},
-	})
-}
-func Setting(c *gin.Context) {
-	session := sessions.Get(c)
-	var name string
-	user := models.User{}
-	//var err error
+func SendMailViaTLS(addr string, auth smtp.Auth, from string,
+	to []string, msg []byte) (err error) {
 
-	if nil != session.Get("loginname") {
-		name = session.Get("loginname").(string)
-		user, _ = userModel.GetUserByName(name)
+	c, err := Dial(addr)
+	if err != nil {
+		log.Println("Create SMTP Client fail:", err)
+		return err
 	}
-	log.Println(user)
-	c.HTML(http.StatusOK, "setting", gin.H{
-		"title": "布局页面",
-		"user":  user,
-		"config": gin.H{
-			"description": "CNode：Node.js专业中文社区",
-		},
-	})
-}
-func Message(c *gin.Context) {
-	session := sessions.Get(c)
-	var name string
-	user := models.User{}
-	//var err error
+	defer c.Close()
 
-	if nil != session.Get("loginname") {
-		name = session.Get("loginname").(string)
-		user, _ = userModel.GetUserByName(name)
-	}
-	log.Println(user)
-	c.HTML(http.StatusOK, "message_index", gin.H{
-		"title": "布局页面",
-		"user":  user,
-		"config": gin.H{
-			"description": "CNode：Node.js专业中文社区",
-		},
-	})
-}
-func Signup(c *gin.Context) {
-	var msg string
-	/* 	session := sessions.Get(c)
-	   	var name string
-	   	user := models.User{}
-	   	//var err error
-
-	   	if nil != session.Get("loginname") {
-	   		name = session.Get("loginname").(string)
-	   		user, _ = userModel.GetUserByName(name)
-	   	}
-		   log.Println(user) */
-	loginname := c.Request.FormValue("loginname")
-	email := c.Request.FormValue("email")
-	pass := c.Request.FormValue("pass")
-	rePass := c.Request.FormValue("re_pass")
-	pat := `^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$`
-	reg, _ := regexp.Match(pat, []byte(email))
-	if loginname == "" || email == "" || pass == "" || rePass == "" {
-		msg = "信息不完整。"
-	} else if len(loginname) < 5 {
-		msg = "用户名至少需要5个字符。"
-	} else if rePass != pass {
-		msg = "两次密码输入不一致。"
-	} else if !reg {
-		msg = "邮箱不合法。"
+	if auth != nil {
+		if ok, _ := c.Extension("AUTH"); ok {
+			if err = c.Auth(auth); err != nil {
+				log.Println("AUTH ERROR ", err)
+				return err
+			}
+		}
 	}
 
-	log.Print(reg)
-	if msg != "" {
-		c.HTML(http.StatusOK, "signup", gin.H{
-			"title": "布局页面",
-			// "user":  user,
-			"error":     msg,
-			"loginname": loginname,
-			"email":     email,
-			"config": gin.H{
-				"description": "CNode：Node.js专业中文社区",
-			},
-		})
-		return
+	if err = c.Mail(from); err != nil {
+		return err
 	}
-	c.HTML(http.StatusOK, "signup", gin.H{
-		"title": "布局页面",
-		// "user":  user,
-		"success":   "欢迎加入 ！我们已给您的注册邮箱发送了一封邮件，请点击里面的链接来激活您的帐号。",
-		"loginname": loginname,
-		"email":     email,
-		"config": gin.H{
-			"description": "CNode：Node.js专业中文社区",
-		},
-	})
 
+	for _, addr := range to {
+		if err = c.Rcpt(addr); err != nil {
+			return err
+		}
+	}
+
+	w, err := c.Data()
+	if err != nil {
+		return err
+	}
+
+	_, err = w.Write(msg)
+	if err != nil {
+		return err
+	}
+
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+
+	return c.Quit()
 }
+
+
+
+
+
+
+
+
 func SendActiveMail(who string, token string, name string) {
 	auth := smtp.PlainAuth("", "dangyanglim@qq.com", "uicmeimalcnybgdj", "smtp.qq.com")
 	to := []string{who}
@@ -185,7 +100,14 @@ func SendActiveMail(who string, token string, name string) {
 		"<p>Go_Cnode社区 谨上。</p>"
 	msg := []byte("To: " + strings.Join(to, ",") + "\r\nFrom: " + nickname +
 		"<" + user + ">\r\nSubject: " + subject + "\r\n" + content_type + "\r\n\r\n" + body)
-	err := smtp.SendMail("smtp.qq.com:25", auth, user, to, msg)
+	//err := smtp.SendMail("smtp.qq.com:465", auth, user, to, msg)
+	err := SendMailViaTLS(
+		"smtp.qq.com:465",
+		auth,
+		user,
+		to,
+		msg,
+	)	
 	if err != nil {
 		fmt.Printf("send mail error: %v", err)
 	}
